08b54437b586b0f101c81e33565aea565129484d
eamonge <39276293+eamonge@users.noreply.github.com>
2020-05-19 23:01:45 -0600

    Proyecto Final Servicios Web

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //metadata
+  templateUrl: './app.component.html', //view
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Proyecto Final';
+}
